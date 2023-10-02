cad1ec0c70c3743545b378f2716400fdb82297f9
pierrelasse <pierrelassebusiness@gmail.com>
2023-10-02 13:11:52 +0200

    Add get_all_weapons() to yimutils

--- yimutils.lua
+++ yimutils.lua
@@ -246,8 +246,12 @@ return (function()
             return api.get_unique_number()
         end
 
-        api.playerindex = function()
-            -- return globals.get_int(1574918)
+        api.playerindex = function(method)
+            if method == 1 then
+                return globals.get_int(1574918) 
+            elseif method == 2 then
+                return globals.get_int(1574907)
+            end
             return stats.get_int("MPPLY_LAST_MP_CHAR")
         end
 
@@ -305,6 +309,108 @@ return (function()
                 end
             end
             return players
+        end
+
+        api.get_all_weapons = function()
+            return {
+                ["dagger"] = 0x92A27487,
+                ["bat"] = 0x958A4A8F,
+                ["bottle"] = 0xF9E6AA4B,
+                ["crowbar"] = 0x84BD7BFD,
+                ["flashlight"] = 0x8BB05FD7,
+                ["golfclub"] = 0x440E4788,
+                ["hammer"] = 0x4E875F73,
+                ["hatchet"] = 0xF9DCBF2D,
+                ["knuckle"] = 0xD8DF3C3C,
+                ["knife"] = 0x99B507EA,
+                ["machete"] = 0xDD5DF8D9,
+                ["switchblade"] = 0xDFE37640,
+                ["nightstick"] = 0x678B81B1,
+                ["wrench"] = 0x19044EE0,
+                ["battleaxe"] = 0xCD274149,
+                ["poolcue"] = 0x94117305,
+                ["stone_hatchet"] = 0x3813FC08,
+                ["pistol"] = 0x1B06D571,
+                ["pistol_mk2"] = 0xBFE256D4,
+                ["combatpistol"] = 0x5EF9FEC4,
+                ["appistol"] = 0x22D8FE39,
+                ["stungun"] = 0x3656C8C1,
+                ["pistol50"] = 0x99AEEB3B,
+                ["snspistol"] = 0xBFD21232,
+                ["snspistol_mk2"] = 0x88374054,
+                ["heavypistol"] = 0xD205520E,
+                ["vintagepistol"] = 0x83839C4,
+                ["flaregun"] = 0x47757124,
+                ["marksmanpistol"] = 0xDC4DB296,
+                ["revolver"] = 0xC1B3C3D1,
+                ["revolver_mk2"] = 0xCB96392F,
+                ["doubleaction"] = 0x97EA20B8,
+                ["raypistol"] = 0xAF3696A1,
+                ["ceramicpistol"] = 0x2B5EF5EC,
+                ["navyrevolver"] = 0x917F6C8C,
+                ["microsmg"] = 0x13532244,
+                ["smg"] = 0x2BE6766B,
+                ["smg_mk2"] = 0x78A97CD0,
+                ["assaultsmg"] = 0xEFE7E2DF,
+                ["combatpdw"] = 0xA3D4D34,
+                ["machinepistol"] = 0xDB1AA450,
+                ["minismg"] = 0xBD248B55,
+                ["raycarbine"] = 0x476BF155,
+                ["pumpshotgun"] = 0x1D073A89,
+                ["pumpshotgun_mk2"] = 0x555AF99A,
+                ["sawnoffshotgun"] = 0x7846A318,
+                ["assaultshotgun"] = 0xE284C527,
+                ["bullpupshotgun"] = 0x9D61E50F,
+                ["musket"] = 0xA89CB99E,
+                ["heavyshotgun"] = 0x3AABBBAA,
+                ["dbshotgun"] = 0xEF951FBB,
+                ["autoshotgun"] = 0x12E82D3D,
+                ["assaultrifle"] = 0xBFEFFF6D,
+                ["assaultrifle_mk2"] = 0x394F415C,
+                ["carbinerifle"] = 0x83BF0278,
+                ["carbinerifle_mk2"] = 0xFAD1F1C9,
+                ["advancedrifle"] = 0xAF113F99,
+                ["specialcarbine"] = 0xC0A3098D,
+                ["specialcarbine_mk2"] = 0x969C3D67,
+                ["bullpuprifle"] = 0x7F229F94,
+                ["bullpuprifle_mk2"] = 0x84D6FAFD,
+                ["compactrifle"] = 0x624FE830,
+                ["mg"] = 0x9D07F764,
+                ["combatmg"] = 0x7FD62962,
+                ["combatmg_mk2"] = 0xDBBD7280,
+                ["gusenberg"] = 0x61012683,
+                ["sniperrifle"] = 0x5FC3C11,
+                ["heavysniper"] = 0xC472FE2,
+                ["heavysniper_mk2"] = 0xA914799,
+                ["marksmanrifle"] = 0xC734385A,
+                ["marksmanrifle_mk2"] = 0x6A6C02E0,
+                ["rpg"] = 0xB1CA77B1,
+                ["grenadelauncher"] = 0xA284510B,
+                ["grenadelauncher_smoke"] = 0x4DD2DC56,
+                ["minigun"] = 0x42BF8A85,
+                ["firework"] = 0x7F7497E5,
+                ["railgun"] = 0x6D544C99,
+                ["hominglauncher"] = 0x63AB0442,
+                ["compactlauncher"] = 0x781FE4A,
+                ["rayminigun"] = 0xB62D1F67,
+                ["grenade"] = 0x93E220BD,
+                ["bzgas"] = 0xA0973D5E,
+                ["smokegrenade"] = 0xFDBC8A50,
+                ["flare"] = 0x497FACC3,
+                ["molotov"] = 0x24B17070,
+                ["stickybomb"] = 0x2C3731D9,
+                ["proxmine"] = 0xAB564B93,
+                ["snowball"] = 0x787F0BB,
+                ["pipebomb"] = 0xBA45E8B8,
+                ["ball"] = 0x23C9F95C,
+                ["petrolcan"] = 0x34A67B97,
+                ["fireextinguisher"] = 0x60EC506,
+                ["parachute"] = 0xFBAB5776,
+                ["electric_fence"] = 0x711D4738,
+                ["plunger"] = 0x2D4239F,
+                ["cattleprod"] = 0x84D676D4,
+                ["unarmed"] = 0xA2719263
+            }
         end
     end
 
