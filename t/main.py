5617c634e1d61be65132a60ad2216b2257f9d402
pierrelasse <pierrelassebusiness@gmail.com>
2023-11-10 17:05:32 +0100

    refactor(t): improve code

--- t/main.py
+++ t/main.py
@@ -10,6 +10,39 @@ def normpath(path):
 def printHelp():
     print("<help>")
 
+def convertLines(lines, config={}):
+    lineCount = len(lines)
+
+    if lineCount <= 7 or not isinstance(config, dict):
+        raise Exception("Invalid args")
+
+    type = lines[0].split(" ")
+    if len(type) < 2:
+        raise Exception("Content has no type specified")
+    type = type[1]
+
+    if type == "SussySpt":
+        lines[4] = lines[4].replace("0--[[VERSIONTYPE]]", str(config["versionType"]))
+        lines[5] = lines[5].replace("0--[[BUILD]]", str(time.time()).split(".")[0])
+
+    elif type == "yimutils":
+        print("Yimutils!")
+
+    else:
+        raise Exception("Content has an invalid type")
+
+    return lines
+
+def increaseSussySptVersionId():
+    with open("SussySpt.lua", "r+") as f:
+        lines = f.readlines()
+        if len(lines) >= 4:
+            lines[3] = "    versionid = {},\n".format(int(lines[3].split()[-1][:-1]) + 1)
+            f.seek(0)
+            f.writelines(lines)
+            f.truncate()
+        return lines
+
 def main():
     args = sys.argv
     root = normpath(__file__ + "/../..")
@@ -36,21 +69,16 @@ def main():
         cmd = args[1]
 
         if cmd == "u" or cmd == "update":
+            with open(root + "/out/SussySpt.luao", "w") as f:
+                f.writelines(
+                    convertLines(increaseSussySptVersionId(), {
+                        "versionType": 2
+                    })
+                )
+
+            shutil.copy(root + "/out/SussySpt.luao", cfg["scriptsPath"] + "/SussySpt.lua")
             shutil.copy(root + "/yimutils.lua", cfg["scriptsPath"] + "/yimutils.lua")
 
-            with open("SussySpt.lua", "r+") as f:
-                lines = f.readlines()
-                if len(lines) >= 3:
-                    lines[2] = f"    versionid = {int(lines[2].split()[-1][:-1]) + 1},\n"
-                    f.seek(0)
-                    f.writelines(lines)
-                    f.truncate()
-
-                    with open(cfg["scriptsPath"] + "/SussySpt.lua", "w") as f2:
-                        if len(lines) >= 5:
-                            lines[3] = lines[3].replace("0--[[VERSIONTYPE]]", "2")
-                            lines[4] = lines[4].replace("0--[[BUILD]]", str(time.time()).split(".")[0])
-                            f2.writelines(lines)
         else:
             printHelp()
 
