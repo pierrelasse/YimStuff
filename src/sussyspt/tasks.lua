63329ff2719dce65783f34650a1cff639a41846e
pierrelasse <pierrelassebusiness@gmail.com>
2024-05-12 10:33:25 +0200

    [SussySpt] fix(tasks): programing is fun

--- src/sussyspt/tasks.lua
+++ src/sussyspt/tasks.lua
@@ -9,13 +9,13 @@ function exports.addTask(cb)
 end
 
 function exports.runAll(rs)
-    for k, v in pairs(exports.tasks) do
-        local success, result = pcall(v, rs)
+    for taskId, taskFunc in pairs(yu.copy_table(exports.tasks)) do
+        exports.tasks[taskId] = nil
+        local success, result = pcall(taskFunc, rs)
         if not success then
-            log.warning("Error while executing task #'"..k.."': "..result)
+            log.warning("Error while executing task #'"..taskId.."': "..result)
             yu.notify(3, "Error executing task. See console for more details")
         end
-        exports.tasks[k] = nil
     end
 end
 
